ddefb0d190efcb9b29a3f38de680eee8a19b13b2
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-15 12:07:46 -0500

    Added game function to allow 5 play rounds

--- script.js
+++ script.js
@@ -28,19 +28,43 @@ function playRound(playerSelection, computerSelection) {
 
     if(compare[playerSelection].weakTo === computerSelection) {
         alert(`You Lose! ${computerSelection} beats ${playerSelection}!`);
+        return "lose";
     }
 
     else if(compare[playerSelection].strongTo === computerSelection) {
         alert(`You WIN! ${playerSelection} beats ${computerSelection}!`)
+        return "win";
     }
 
     else {
         alert("Tie!!");
+        return "tie";
     }
 }
 
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+//Play 5 rounds game.
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+        let outcome = playRound(playerSelection, computerSelection);
+        switch(outcome) {
+            case "win":
+                playerScore++;
+                break;
+            case "lose":
+                computerScore++;
+                break;
+            case "tie":
+                break;
+        }
+        console.log("player score: " + playerScore);
+        console.log("computer score: " + computerScore);
+    }
+    const winner = playerScore > computerScore?"Player won!":"Computer won!";
+    console.log(winner);
+}
+
+game();
