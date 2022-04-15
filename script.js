c735ff5338f8e4be83ce518fa80efc5e1fdc028a
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-15 11:07:54 -0500

    Added function to compare

--- script.js
+++ script.js
@@ -10,5 +10,29 @@ function computerPlay() {
             return "scissors";
     }
 }
+
+console.log(computerPlay());
 //User pick rock, scissor or paper.
+    //--Prompt to ask user to enter rock, scissor or paper.
+    //--Convert player's selection to lowercase to enable comparison.
+
 //Compare the user pick and computer pick, determine who wins.
+function playRound(playerSelection, computerSelection) {
+    const compare = {
+        rock: {weakTo: 'paper' strongTo: 'scissors'}
+        paper: {weakTo: 'scissors' strongTo: 'rock'}
+        scissors: {weekTo: 'rock' strongTo: 'paper'}
+    }
+
+    if(compare[playerSelection].weakTo === computerSelection) {
+        alert(`You Lose! ${computerSelection} beats ${playerSelection}!`);
+    }
+
+    else if(compare[playerSelection].strongTo === computerSelection) {
+        alert(`You WIN! ${playerSelection} beats ${computerSelection}!`)
+    }
+
+    else {
+        alert("You entered something weird...");
+    }
+}
