a30cdd0cfbdcd09ffe999b84794f1f4209c03cd0
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-15 10:24:52 -0500

    Added computerPlay function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //Computer generate a random number, correspond to rock, scissor, paper.
+function computerPlay() {
+    let rand = Math.floor(Math.random()*3);
+    switch(rand) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
 //User pick rock, scissor or paper.
 //Compare the user pick and computer pick, determine who wins.
-//--
